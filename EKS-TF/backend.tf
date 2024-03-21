769929b8b5dab914d29945f5056e4bcdeefce02f
sarapubg1608 <143368620+sarapubg1608@users.noreply.github.com>
2024-03-21 11:52:55 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "vensamplesupermario" # Replace with your actual S3 bucket name
+    bucket = "mydemo-venkata" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
